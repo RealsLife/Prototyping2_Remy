c3e9df52a6bb26d68a23f6204206c45db925ed0e
Dieter-Boddin <57953402+Dieter-Boddin@users.noreply.github.com>
2020-04-12 21:50:07 +0200

    Added menu prefabs, level end beam particle
    + Added: UI elements
    + Added: UI prefabs for each menu (Main, Pause, Profile, Shop)
    + Added: Confirmation box for pause and quit menu
    + Added: Level end beam particles
    + Added: Particle textures (Glow, Beam, Chip)
    + Added: Particle rotation script for fake Chip particle

---
+++ Assets/Scripts/ParticleRotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleRotate : MonoBehaviour
+{
+    [SerializeField]
+    private float _rotationSpeed = 10f;
+
+    private void Update()
+    {
+        this.transform.Rotate(_rotationSpeed * Vector3.up * Time.deltaTime, Space.Self);
+    }
+}
