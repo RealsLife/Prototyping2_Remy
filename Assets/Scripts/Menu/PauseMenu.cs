19fa8f155be4d9195afaee5a1d4591f9e49e3b10
Dieter-Boddin <57953402+Dieter-Boddin@users.noreply.github.com>
2020-04-20 14:04:25 +0200

    Smoke particle, full menu UI and functionality
    • Profilepivot into prefab named ProfileHeader
    • Visual setup in scenes: Mainmenu, Shop, Profile and linking button functionality
    • Some buttons now use a grid layout
    • Start screen visual + functionality
    • Login screen visual + functionality
    • Confirmation box functionalities (Main menu quit, Pause screen)
    • Pause screen + mouse lockstates
    
    • Smoke particle system + cloud model

---
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject _menu;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+            ToggleMenu();
+    }
+
+    public void ToggleMenu()
+    {
+        _menu.SetActive(!_menu.activeSelf);
+        HandleCursorMode();
+    }
+
+    private void HandleCursorMode()
+    {
+        if (_menu.activeSelf)
+            Cursor.lockState = CursorLockMode.None;
+        else if (!_menu.activeSelf)
+            Cursor.lockState = CursorLockMode.Locked;
+    }
+}
---
+++ Assets/Scripts/Menu/ProfileHeader.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProfileHeader : MonoBehaviour
+{
+    [SerializeField] private Text _playerNameTextField;
+
+    private void Start()
+    {
+        _playerNameTextField.text = PlayerProfile._playerName;
+    }
+}
