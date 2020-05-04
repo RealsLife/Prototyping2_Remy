f6fb44845a329e2beebe963ae9de48d7591222de
Jan-Pieter Inghels (desktop) <43540832+Jan-PieterInghels@users.noreply.github.com>
2020-05-04 12:27:26 +0200

    Implemented dynamic CarSpawner functionality for bend tiles

--- Assets/Scripts/CarSpawnerController.cs
+++ Assets/Scripts/CarSpawnerController.cs
@@ -17,21 +17,26 @@ public class CarSpawnerController : MonoBehaviour
             _currentTileRotation = (int)gameObject.transform.parent.rotation.eulerAngles.y;
             _currentTileZPosition = (int)gameObject.transform.parent.position.z;
             _currentTileXPosition = (int)gameObject.transform.parent.position.x;
+
             
-            if(_tile.Type == Tile.TileType.Straight)
+
+            if (_tile.Type == Tile.TileType.Straight)
             {
                 EnableCarSpawnersForStraightTile();
+            }
+            else if(_tile.Type == Tile.TileType.Bend)
+            {
+                EnableCarSpawnersForBendTile();
+                //Debug.Log(gameObject.transform.parent.name);
+                //Debug.Log(_currentTileRotation);
+                //Debug.Log(_currentTileZPosition);
+                //Debug.Log(_currentTileXPosition);
             }
         }
     }
 
     private void EnableCarSpawnersForStraightTile()
     {
-        //Debug.Log(gameObject.transform.parent.name);
-        //Debug.Log(_currentTileRotation);
-        //Debug.Log(_currentTileZPosition);
-        //Debug.Log(_currentTileXPosition);
-
         if (_currentTileRotation == 0)
         {
             if(_currentTileZPosition < 0)
@@ -74,6 +79,62 @@ public class CarSpawnerController : MonoBehaviour
             else
             {
                 CarSpawnerArray[2].gameObject.SetActive(true);
+            }
+        }
+    }
+
+    private void EnableCarSpawnersForBendTile()
+    {
+        if (_currentTileRotation == 0)
+        {
+            if (_currentTileXPosition < 0)
+            {
+                CarSpawnerArray[3].gameObject.SetActive(true);
+            }
+            else
+            {
+                CarSpawnerArray[0].gameObject.SetActive(true);
+            }
+        }
+        else if (_currentTileRotation == 90)
+        {
+            if (_currentTileZPosition > 0)
+            {
+                CarSpawnerArray[3].gameObject.SetActive(true);
+            }
+            else
+            {
+                CarSpawnerArray[0].gameObject.SetActive(true);
+            }
+        }
+        else if (_currentTileRotation == 180)
+        {
+            if (_currentTileXPosition < 0)
+            {
+                CarSpawnerArray[0].gameObject.SetActive(true);
+            }
+            else if(_currentTileXPosition > 0)
+            {
+                CarSpawnerArray[3].gameObject.SetActive(true);
+            }
+            else if(_currentTileZPosition > 0)
+            {
+                CarSpawnerArray[0].gameObject.SetActive(true);
+            }
+            else if(_currentTileZPosition < 0)
+            {
+                CarSpawnerArray[3].gameObject.SetActive(true);
+            }
+        }
+        else if(_currentTileRotation == 270)
+        {
+            if(_currentTileZPosition < 0)
+            {
+                CarSpawnerArray[3].gameObject.SetActive(true);
+            }
+            else
+            {
+                CarSpawnerArray[0].gameObject.SetActive(true);
             }
         }
     }
