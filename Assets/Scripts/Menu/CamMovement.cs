edf9a93df9f6ea2cde16745b0d0408e1faf1ef00
BrianWouters <brian.wouters@hotmail.com>
2020-05-07 08:07:30 +0200

    Parallax system+startscreen

---
+++ Assets/Scripts/Menu/CamMovement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMovement : MonoBehaviour
+{
+    int speed = 3;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position += new Vector3(speed * Time.deltaTime, 0, 0) ;
+    }
+}
---
+++ Assets/Scripts/Menu/ParallaxBackground.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Parallax scrolling script that should be assigned to a layer
+/// </summary>
+public class ParallaxBackground : MonoBehaviour
+{
+    public RawImage img;
+    public float speed;
+    private Rect rect;
+
+    private void Start()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        Rect rect = img.uvRect;
+        rect.x += speed * Time.deltaTime;
+        rect.x =(float)System.Math.Round(rect.x, 4);
+
+        img.uvRect = rect;
+    }
+}
