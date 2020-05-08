c9b8278155e150a0f7a987f0defb7fb8c178fc5d
LouArth <49731340+LouArth@users.noreply.github.com>
2020-05-08 11:20:59 +0200

    Added bigger tiles, Tutorial and Dialog

---
+++ Assets/Scripts/Dialog.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialog : MonoBehaviour
+{
+    public TextMeshProUGUI TextDisplay;
+    public string[] Sentences;
+    public float TypingSpeed;
+    public GameObject ContinueButton;
+
+    private int _index;
+
+    private void Start()
+    {
+
+        StartCoroutine(Type());
+    }
+    private void Update()
+    {
+        if(TextDisplay.text == Sentences[_index])
+        {
+            ContinueButton.SetActive(true);
+        }
+    }
+    IEnumerator Type()
+    {
+        foreach(char letter in Sentences[_index].ToCharArray())
+        {
+            TextDisplay.text += letter;
+            yield return new WaitForSeconds(TypingSpeed);
+        }
+    }
+    public void NextSentence()
+    {
+        ContinueButton.SetActive(false);
+        if (_index < Sentences.Length - 1)
+        {
+            _index++;
+            TextDisplay.text = "";
+            StartCoroutine(Type());
+        }
+        else
+        {
+            TextDisplay.text = "";
+            ContinueButton.SetActive(false);
+
+        }
+          
+    }
+}
