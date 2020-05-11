c6b9b496e6c047897144fd71bc9be699fa51243f
BrianWouters <brian.wouters@hotmail.com>
2020-05-11 10:07:36 +0200

    UI part 3

--- Assets/Scripts/Menu/ProfileHeader.cs
+++ Assets/Scripts/Menu/ProfileHeader.cs
@@ -1,14 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ProfileHeader : MonoBehaviour
 {
-    [SerializeField] private Text _playerNameTextField;
-
+    [SerializeField] private TextMeshProUGUI _playerNameTextField;
+    [SerializeField] private TextMeshProUGUI _genderField;
+    //[SerializeField] private TextMeshPro _playerNameTextField;
+    //[SerializeField] private TextMeshPro _playerNameTextField;
+    //[SerializeField] private TextMeshPro _playerNameTextField;
     private void Start()
     {
         _playerNameTextField.text = ProfileSaver._playerProfile._name;
+        if (ProfileSaver._playerProfile._gender == 0)
+        {
+            _genderField.text = "Jongen";
+        }
+        else
+        {
+            _genderField.text = "Meisje";
+        }
+       
     }
 }
