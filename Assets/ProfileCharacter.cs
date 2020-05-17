e825e99cf1678f9de427c32ef45d083070e429a5
LouArth <49731340+LouArth@users.noreply.github.com>
2020-05-17 17:59:58 +0200

    Edit boy girl prefab

---
+++ Assets/ProfileCharacter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProfileCharacter : MonoBehaviour
+{
+    [SerializeField] private GameObject _PlayerPrefabBoy;
+    [SerializeField] private GameObject _PlayerPrefabGirl;
+    enum Gender
+    {
+        Boy = 0,
+        Girl = 1,
+    }
+
+    private void Update()
+    {
+        SpawnPlayer();
+    }
+    public void SpawnPlayer()
+    {
+        if (ProfileSaver._playerProfile._gender == (int)Gender.Boy)
+        {
+            _PlayerPrefabBoy.SetActive(true);
+            _PlayerPrefabGirl.SetActive(false);
+        }
+        else if (ProfileSaver._playerProfile._gender == (int)Gender.Girl)
+        {
+            _PlayerPrefabBoy.SetActive(false);
+            _PlayerPrefabGirl.SetActive(true);
+        }
+        else
+        {
+            _PlayerPrefabBoy.SetActive(true);
+            _PlayerPrefabGirl.SetActive(false);
+
+        }
+    }
+}
